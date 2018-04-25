f1e776d7dbbccacc6de53090c0a9603d0ea7bfa5
Chris Core <chris.core@crispthinking.com>
2018-04-25 15:14:38 +0100

    Added splash client, request options classes and JSON converter

---
+++ SplashSharp/Serialization/NullableBoolJsonConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SplashSharp.Serialization
+{
+    public class NullableBoolConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var asBool = (bool?)value;
+
+            if (asBool == null) writer.WriteNull();
+            else
+            {
+                if (asBool.Value) writer.WriteValue(1);
+                else writer.WriteValue(0);
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.Value == null) return null;
+            else if (reader.Value.ToString().Equals("1")) return true;
+            else if (reader.Value.ToString().Equals("0")) return false;
+
+            throw new JsonException($"Could not read value '{reader.Value.ToString()}' as nullable bool");
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(bool?);
+        }
+    }
+}
---
+++ SplashSharp/SplashClient.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using SplashSharp.Requests;
+using SplashSharp.Serialization;
+
+namespace SplashSharp
+{
+    public class SplashClient
+    {
+        public SplashClient(string splashBaseUrl)
+            : this(splashBaseUrl, new HttpClient())
+        { }
+
+        public SplashClient(string splashBaseUrl, HttpClient client)
+        {
+            Client = client;
+            SplashBaseUrl = splashBaseUrl;
+
+            JsonSettings = new JsonSerializerSettings()
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                ContractResolver = new DefaultContractResolver()
+                {
+                    NamingStrategy = new SnakeCaseNamingStrategy(false, true)
+                }
+            };
+
+            JsonSettings.Converters.Add(new NullableBoolConverter());
+        }
+
+        public string SplashBaseUrl { get; }
+
+        private HttpClient Client { get; }
+        private JsonSerializerSettings JsonSettings { get; }
+
+        private const string RenderHtmlEndpoint = "render.html";
+
+        public Task<HttpResponseMessage> GetHtmlAsync(RenderHtmlOptions options, CancellationToken token = default(CancellationToken))
+        {
+            var request = BuildSplashRequest(RenderHtmlEndpoint, options);
+            return Client.SendAsync(request, token);
+        }
+
+        public async Task<HtmlDocument> GetHtmlDocumentAsync(RenderHtmlOptions options, CancellationToken token = default(CancellationToken))
+        {
+            var document = new HtmlDocument();
+
+            // TODO: check for response error
+            var response = await GetHtmlAsync(options, token);
+            var responseStream = await response.Content.ReadAsStreamAsync();
+
+            document.Load(responseStream);
+            return document;
+        }
+
+        private HttpRequestMessage BuildSplashRequest(string endpoint, BaseSplashRequestOptions options)
+        {
+            var builder = new UriBuilder(SplashBaseUrl) {Path = endpoint};
+
+            var requestUri = builder.ToString();
+            var request = new HttpRequestMessage(HttpMethod.Post, requestUri)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(options, JsonSettings), Encoding.UTF8, "application/json")
+            };
+
+            return request;
+        }
+    }
+}
